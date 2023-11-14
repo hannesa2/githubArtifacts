5e0fe245e116acc743e8e5569fb398895589dbd3
Hannes Achleitner <hannes.software@gmx.at>
2023-11-14 06:54:44 +0100

    Customer routes

--- src/main/kotlin/hannes/info/plugins/CustomerRoutes.kt
+++ src/main/kotlin/hannes/info/plugins/CustomerRoutes.kt
@@ -3,51 +3,31 @@ package hannes.info.plugins
 import hannes.info.Customer
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.request.*
 
-fun Application.configureRouting() {
-
+fun Route.customerRouting() {
     val customerStorage = mutableListOf<Customer>()
 
-    install(StatusPages) {
-        exception<IllegalStateException> { call, cause ->
-            call.respondText("App in illegal state as ${cause.message}")
+    route("/customer") {
+        get {
+            if (customerStorage.isNotEmpty()) {
+                call.respond(customerStorage)
+            } else {
+                call.respondText("No customers found", status = HttpStatusCode.OK)
+            }
         }
-    }
-
-    routing {
-        staticResources(
-            "/content",
-            "mycontent"
-        )
-
-        customerRouting()
-
-        get("/") {
-            call.respondText("Hello World from /!")
-        }
-        get("/error-test") {
-            throw IllegalStateException("Too Busy")
-        }
-        get("/test1") {
-            val text = "<h1>Hello From Ktor</h1>"
-            val type = ContentType.parse("text/html")
-            call.respondText(text, type)
-        }
-
         get("{id?}") {
             val id = call.parameters["id"] ?: return@get call.respondText(
                 "Missing id",
                 status = HttpStatusCode.BadRequest
             )
-            val customer = customerStorage.find { it.id == id } ?: return@get call.respondText(
-                "No customer with id $id",
-                status = HttpStatusCode.NotFound
-            )
+            val customer =
+                customerStorage.find { it.id == id } ?: return@get call.respondText(
+                    "No customer with id $id",
+                    status = HttpStatusCode.NotFound
+                )
             call.respond(customer)
         }
         post {
